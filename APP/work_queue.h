3ee1f6c53e18962ecef2d762a87c440a20bc7261
laobanwang676-droid <laobanwang676@gmail.com>
2026-05-22 13:01:13 +0800

    first

--- APP/work_queue.h
+++
@@ -1,8 +0,0 @@
-#ifndef WORK_QUEUE_H
-#define WORK_QUEUE_H
-
-typedef void (*WorkFunction_t)(void *param);
-void work_queue_init(void);
-void work_send(WorkFunction_t function, void *param);
-
-#endif /* WORK_QUEUE_H */
